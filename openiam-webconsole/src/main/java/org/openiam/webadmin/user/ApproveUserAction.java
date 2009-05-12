27a3dfc28cbd8788928e92da50b27f5a2894aa46
Suneet Shah <suneetshah2000@gmail.com>
2009-05-12 21:52:52 +0000

    Corrected bugs in ApproveUserAction

--- openiam-webconsole/src/main/java/org/openiam/webadmin/user/ApproveUserAction.java
+++ openiam-webconsole/src/main/java/org/openiam/webadmin/user/ApproveUserAction.java
@@ -48,11 +48,7 @@ public class ApproveUserAction extends NavigationDispatchAction {
 
 	// --------------------------------------------------------- Instance Variables
 
-	//AuditLogAccess logAccess = null;
-	//UserAccess userAccess = null;
-	//protected JMSSender sender;
-	
-	//private EmailManager emailManager;
+	UserDataService userMgr; 
 	
 	// --------------------------------------------------------- Methods
 
@@ -72,26 +68,13 @@ public class ApproveUserAction extends NavigationDispatchAction {
 		throws IOException, ServletException {
 		
 		List filteredList = new ArrayList();
-		
-		WebApplicationContext webContext =  getWebApplicationContext();
-		UserDataService userMgr = (UserDataService)webContext.getBean("userManager");
-		
+	
 
 		try {
-	        //HttpSession session = request.getSession();          
-	        //UserAccess userAccess = new UserAccess();
-	        //String userId = (String)session.getAttribute("userId");
-	        
-	        // create the search object
-	       // UserSearch search = new UserSearch();
-	        //search.setStatus("PENDING");
-	        //List userList = userAccess.findUser(search);
+
 	        
 	        List userList = userMgr.findUsersByStatus("PENDING");
-	        
-	      //  System.out.println("view userlist = " + userList);
-	      //  System.out.println("view userlist size= " + userList.size());
-	        
+      
 	        request.setAttribute("userList", userList);
         
 		}catch(Exception e) {
@@ -108,17 +91,11 @@ public class ApproveUserAction extends NavigationDispatchAction {
 	
 	HttpSession session = request.getSession();
 	try {
-        UserAccess userAccess = new UserAccess();
         DynaValidatorForm dynForm = (DynaValidatorForm) form;
-        //String userId = (String)dynForm.get("uId");
         String[] personId = request.getParameterValues("personId");
         
         String task = (String)dynForm.get("submit");
  
-		WebApplicationContext webContext =  getWebApplicationContext();
-        IdmAuditLogDataService auditService = 
- 	    		 (IdmAuditLogDataService)webContext.getBean("auditDataService");
-
         
        
         int len = personId.length;
@@ -128,57 +105,21 @@ public class ApproveUserAction extends NavigationDispatchAction {
             request.setAttribute("COUNT",new Integer(0));  
             return mapping.findForward("success"); 
         }
-        
-        
+               
         if (task.equalsIgnoreCase("Reject")) {
         	session.setAttribute("personId", personId);
         	return mapping.findForward("reject");
-        	//session.setAttribute("selectedPersons", personId);
-        	//return mapping.findForward("reject"); 
+
         }else {
         	// approved
         	for (int i=0; i<len; i++) {
         		String selectedPersonId = personId[i];
-        		UserData ud = userAccess.getUser(selectedPersonId);
-        		ud.setStatusId("APPROVED");
-        		userAccess.saveUser(ud);
-        		// log the approval
-        	//	logEvent("User id=" + selectedPersonId + " has been approved",
-        	//			request.getRemoteHost(), (String)session.getAttribute("userId"),"IDM");
+        		User usr = userMgr.getUserWithDependent(selectedPersonId, false);
+        		usr.setStatus("APPROVED");
+        		userMgr.updateUser(usr);
 
-      			String logMsg = "User id=" + selectedPersonId + " has been approved";
-                  			             
-      			IdmAuditLog log = new IdmAuditLog();
-      			log.setActionId("APPROVAL");
-      			log.setActionStatus("SUCCESS");
-      			log.setActionDatetime(new Date(System.currentTimeMillis()));
-      			log.setHost(request.getRemoteHost());
-      			log.setLogHash("00");
-      			log.setObjectName(Class.forName("org.openiam.idm.srvc.user.dto.User").getName());
-      			log.setObjectTypeId(selectedPersonId);
-      			log.setReason("USER_APPROVAL");
-      			log.setReasonDetail(logMsg);
-      			log.setReqUrl(request.getRequestURI());
-      			log.setServiceId("IDM");
-      			log.setUserId((String)session.getAttribute("userId"));
-      			auditService.addLog(log);
-        	
-         		
-        		//this.sender.sendMessage(ud);
-        	//	System.out.println("Sending email message...");
-        	//	org.openiam.idm.srvc.user.dto.User user = new org.openiam.idm.srvc.user.dto.User();
-        	//	user.setFirstName(ud.getFirstName());
-        	//	Email email = userAccess.getEmailByName(ud.getId(),"DEFAULT");
-        	//	this.emailManager.sendMail(user, email.getEmailAddress(), true);
-        		
-        		// temp code to write user into ldap
-        	//	Spml2Service ldap = (Spml2Service)getWebApplicationContext().getBean("ldapService");
-        	//	ldap.add(ud.getFirstName()+ud.getLastName(),
-        	//			ud.getFirstName() + " " + ud.getLastName(),
-        	//			ud.getLastName(), ud.getFirstName(), 
-        	//			ud.getFirstName()+"_"+ud.getLastName()+".openiam.org");
-       		
-        		
+        		String logMsg = "User id=" + selectedPersonId + " has been approved";
+   		
         	}
         }
         request.setAttribute("OPERATION", task);
@@ -197,11 +138,7 @@ public class ApproveUserAction extends NavigationDispatchAction {
 
 		UserNote noteValue = null;
 		String logMsg = null;
-
-		WebApplicationContext webContext =  getWebApplicationContext();
-        IdmAuditLogDataService auditService = 
- 	    		 (IdmAuditLogDataService)webContext.getBean("auditDataService");
-		UserDataService userMgr = (UserDataService)webContext.getBean("userManager");        
+ 
 
 		
 		HttpSession session = request.getSession();
@@ -214,60 +151,43 @@ public class ApproveUserAction extends NavigationDispatchAction {
 		String userId = (String)session.getAttribute("userId");
 		String login = (String)session.getAttribute("login");
 		
-		try {
-	       	for (int i=0; i<len; i++) {
-        		String selectedPersonId = personId[i];
-        		User ud = userMgr.getUserWithDependent(selectedPersonId,false);
-        		ud.setStatus("REJECTED");
-        		userMgr.updateUser(ud);
-        		// save the reason for the rejection with the user
-        		noteValue = new UserNote();
-        		noteValue.setCreatedBy(userId);
-        		noteValue.setCreateDate(new Timestamp(System.currentTimeMillis()));
-        		noteValue.setDescription((String)userNotesForm.get("description"));
-        		noteValue.setNoteType(noteType);
-        		noteValue.setUserId(selectedPersonId);
-        		userMgr.addNote(noteValue);
-                
-        		//AuditLogAccess.logEvent("User id=" + selectedPersonId + " has been REJECTED.", 
-                //		request.getRemoteHost(),userId, login,"IDM"); 
-                
-                
-     			logMsg = "User id=" + selectedPersonId + " has been rejected";
-		             
-      			IdmAuditLog log = new IdmAuditLog();
-      			log.setActionId("REJECT");
-      			log.setActionStatus("SUCCESS");
-      			log.setActionDatetime(new Date(System.currentTimeMillis()));
-      			log.setHost(request.getRemoteHost());
-      			log.setLogHash("00");
-      			log.setObjectName(Class.forName("org.openiam.idm.srvc.user.dto.User").getName());
-      			log.setObjectTypeId(selectedPersonId);
-      			log.setReason("USER_REJECT");
-      			log.setReasonDetail(logMsg);
-      			log.setReqUrl(request.getRequestURI());
-      			log.setServiceId("IDM");
-      			log.setUserId((String)session.getAttribute("userId"));
-      			auditService.addLog(log);
 
-        		
-        		//System.out.println("Sending email message...");
-        		org.openiam.idm.srvc.user.dto.User user = new org.openiam.idm.srvc.user.dto.User();
-        		user.setFirstName(ud.getFirstName());
-        	//	Email email = userAccess.getEmailByName(ud.getId(),"DEFAULT");
-        	//	this.emailManager.sendMail(user, email.getEmailAddress(), false);
-       		
-        	}
-		}catch(ClassNotFoundException ce) {
-			Log.error(ce.getMessage(),ce);
-			ce.printStackTrace();	
-		}
+       	for (int i=0; i<len; i++) {
+    		String selectedPersonId = personId[i];
+    		User ud = userMgr.getUserWithDependent(selectedPersonId,false);
+    		ud.setStatus("REJECTED");
+    		userMgr.updateUser(ud);
+    		// save the reason for the rejection with the user
+    		noteValue = new UserNote();
+    		noteValue.setCreatedBy(userId);
+    		noteValue.setCreateDate(new Timestamp(System.currentTimeMillis()));
+    		noteValue.setDescription((String)userNotesForm.get("description"));
+    		noteValue.setNoteType(noteType);
+    		noteValue.setUserId(selectedPersonId);
+    		userMgr.addNote(noteValue);           
+            
+ 			logMsg = "User id=" + selectedPersonId + " has been rejected";
+  		
+    		//System.out.println("Sending email message...");
+    		org.openiam.idm.srvc.user.dto.User user = new org.openiam.idm.srvc.user.dto.User();
+    		user.setFirstName(ud.getFirstName());
+
+    	}
+
         request.setAttribute("OPERATION", "REJECTED");
         request.setAttribute("COUNT",new Integer(len));
         session.removeAttribute("personId");
 		return mapping.findForward("confirm"); 
 
 		
+	}
+
+	public UserDataService getUserMgr() {
+		return userMgr;
+	}
+
+	public void setUserMgr(UserDataService userMgr) {
+		this.userMgr = userMgr;
 	}
 	
 
