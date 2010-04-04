0bb97dbea4e021af918e3d0d2045c7a9ebc89ca8
Suneet Shah <suneetshah2000@gmail.com>
2010-04-04 04:23:21 +0000

    Rmoved to prepare for 2.1 initial load

--- idm-connector-intf/src/main/java/org/openiam/provision/type/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.openiam.org/service/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.openiam.provision.type;
-
